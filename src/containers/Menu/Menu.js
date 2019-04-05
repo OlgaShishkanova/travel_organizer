ea269fabc486b3b26fc1e794a475ad312dfba524
Olga <lili432@mail.ru>
2019-04-05 11:17:33 +0200

    Menu + Settings Template

--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -1,27 +1,47 @@
 import React, {Component} from 'react'
-import {connect} from "react-redux";
-import * as AppActions from "../../actions/AppActions";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCog } from '@fortawesome/free-solid-svg-icons'
-import {bindActionCreators} from "redux";
+import {connect} from "react-redux"
+import * as AppActions from "../../actions/AppActions"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCog, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import {bindActionCreators} from "redux"
+import classNames from 'classnames'
 
 @connect(mapStateToProps, mapDispatchToProps)
 export default class Menu extends Component {
-
+    state={
+        isSettings: false
+    };
 
     render() {
+        const {isSettings} = this.state;
+        let titleText = isSettings ? 'Settings' : 'Travelanner';
         return (
-            <div>
-                <div>
-                    Travelanner
+            <div className='menu'>
+                <div className='menu__title_wrapper'>
+                    <div className='menu__title'>
+                        {titleText}
+                    </div>
                 </div>
-                <div>
-                    <FontAwesomeIcon icon={faCog} color="#FFC45A" size='lg'/>
+                <div className='menu__icon_wrapper' onClick={this.toggleSettings}>
+                    <div className='menu__icon'>
+                        <FontAwesomeIcon icon={isSettings ? faArrowLeft : faCog} color="#FFC45A" size='lg'/>
+                    </div>
+                </div>
+                <div className={classNames('settings', {isOpen: isSettings})}>
+
+                    <div>Всякие настройки</div>
+                    <div>Всякие настройки</div>
+                    <div>Всякие настройки</div>
+                    <div>Всякие настройки</div>
                 </div>
             </div>
         );
     }
-
+    toggleSettings = () =>{
+        this.setState({
+            isSettings: !this.state.isSettings
+        })
+    }
 }
 
 function mapStateToProps(state) {
